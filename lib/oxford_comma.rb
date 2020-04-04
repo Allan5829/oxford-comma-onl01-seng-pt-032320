91d6a8964a89cd922247f9b5b07f647e2e47692f
Allan Farca <allan.farca@gmail.com>
2020-04-04 03:02:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def oxford_comma(array)
   if array.size == 1
     array.join (" ")
@@ -7,3 +8,10 @@ def oxford_comma(array)
    array[0...-1].join(", ") << ", and #{array[-1]}"
   end
 end
+=======
+array = ["fiddleheads","okra","kohlrabi"]
+
+def oxford_comma(array)
+  
+end
+>>>>>>> 7f3b7ec8f0881715c7c2f22c19bdbb6d6d963602
